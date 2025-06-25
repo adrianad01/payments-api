a7f06bfd5cdc9779348cacf5be65172c948be3ca
Jesús Adrián Alcocer Dóñez <jesusadrian@MacBook-Pro-de-Jesus.local>
2025-06-24 19:01:46 -0600

    first commit

---
+++ src/main/java/org/devyadrian/App.java
@@ -0,0 +1,13 @@
+package org.devyadrian;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
