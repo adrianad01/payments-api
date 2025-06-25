8bf8ca99479644c5f338ac1914ae37a94b33d55b
Jesús Adrián Alcocer Dóñez <jesusadrian@MacBook-Pro-de-Jesus.local>
2025-06-24 20:28:17 -0600

    feat: add pom.xml dependencies, init properties and jacoco configurations

---
+++ src/main/java/org/devyadrian/CafeteriaServicesApplication.java
@@ -0,0 +1,16 @@
+package org.devyadrian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This class contains the main application
+ *
+ */
+@SpringBootApplication
+public class CafeteriaServicesApplication
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(CafeteriaServicesApplication.class, args);
+    }
+}
